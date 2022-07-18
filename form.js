931a197158486e2d030752e49e1358078e32611d
Sidhesh Parab <42218395+Everlasting12@users.noreply.github.com>
2022-07-18 21:18:31 +0530

    first commit

---
+++ form.js
@@ -0,0 +1,40 @@
+let formData = document.getElementById("formdata");
+// console.log(formData);
+// let formData1 = document.getElementById("formdata").children;
+// console.log(formData1);
+
+function getElement(eleId) {
+  return document.getElementById(eleId);
+}
+
+function getFormData(formDataEle) {
+  return formData.elements[formDataEle];
+}
+formData.addEventListener("submit", (event) => {
+  event.preventDefault();
+  document.getElementById("table").classList.toggle("displayNone");
+  getElement("displayDate").innerText = getFormData("date").value;
+  getElement("displayFullName").innerText = getFormData("fullname").value;
+  getElement("displayInformation").innerText = getFormData("info").value;
+  getElement("displayEmailId").innerText = getFormData("email").value;
+  getElement("displaySelection").innerText = getFormData("laptop").value;
+  getElement("displayMobileNumber").innerText =
+    getFormData("mobileNumber").value;
+
+  if (getFormData("gender")[0].checked) {
+    getElement("displayGender").innerText = "Male";
+  } else if (getFormData("gender")[1].checked) {
+    getElement("displayGender").innerText = "Female";
+  } else {
+    getElement("displayGender").innerText = "";
+  }
+
+  if (getFormData("terms_condition").checked) {
+    getElement("displayTermsAndCondition").innerText = "Yes";
+  } else {
+    getElement("displayTermsAndCondition").innerText = "No";
+  }
+});
+
+// let promptPassword = prompt("Please Enter the password: ");
+// console.log(promptPassword);
